5ae57403e4fb956d4aef5ee20163446d2385c27d
tmtbe <896369042@qq.com>
2016-04-19 11:49:03 +0800

    Update Redis.php

--- workerman-thrift/Applications/Thrift/Config/Redis.php
+++ workerman-thrift/Applications/Thrift/Config/Redis.php
@@ -8,10 +8,10 @@ class Redis
      * Redis的一个实例配置
      */
     public static $redis1 = array(
-        'host'    => '948155ae86a1483b.m.cnhza.kvstore.aliyuncs.com',
+        'host'    => '***',
         'port'    => 6379,
-        'user'    => '7b1d194800e949b0',
-        'password'=> '7LzxuKrEvvAn1xhh',
+        'user'    => '***',
+        'password'=> '***',
         'select'  => 2,
     );
 }
